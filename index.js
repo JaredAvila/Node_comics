81efcd4ce60a3bfed8f82ea40c99783ac7c70415
Jared Avila <jared.c.avila@gmail.com>
2019-07-05 17:07:53 -0700

    styling the comic template

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+// Templates
+
+const homeTemp = fs.readFileSync(
+  `${__dirname}/templates/home-template.html`,
+  "utf-8"
+);
+
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.end("Welcome to your server! Pretty easy stuff");
+  if (req.url === "/" || req.url === "/home") {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    res.end(homeTemp);
+  } else if (req.url === "/comic") {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    res.end("Comic page");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html"
+    });
+    res.end("<h1>Page not found</h1><br /><a href='/'>Back to home</a>");
+  }
 });
 
 server.listen(port, () => {
